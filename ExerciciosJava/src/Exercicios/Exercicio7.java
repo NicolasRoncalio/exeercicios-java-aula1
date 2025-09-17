7971ef53ed67c4e654aacdd60b5c57a589b58118
Nicolas Roncalio <nicolasroncalio09@gmail.com>
2025-09-16 21:02:10 -0300

    Update Exercicio7.java

--- ExerciciosJava/src/Exercicios/Exercicio7.java
+++ ExerciciosJava/src/Exercicios/Exercicio7.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Exercicios;
 
-/**
- *
- * @author 122317640
- */
+import java.util.Scanner;
+
 public class Exercicio7 {
+    
+    public static void main(String[] args){
+        
+        float valorAluminio = 0.01f;
+        float  raio, altura, custo, pi, area;
+        
+        Scanner entrada = new Scanner(System.in);
+                
+        pi = 3.14f;
+        
+        System.out.println("Digite o raio da lata: ");
+        raio = entrada.nextFloat();
+        
+        System.out.println("Digite a altura da lata: ");
+        altura = entrada.nextFloat();
+        
+        area = 2*pi*raio*(raio + altura);
+        custo = area * valorAluminio;
+        
+        System.out.println("O custo por lata produzida sera de: R$"+ custo);
+        
+    }
     
 }
